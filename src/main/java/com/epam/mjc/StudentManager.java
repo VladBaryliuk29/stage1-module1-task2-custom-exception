d8516e3ca17406a97c01200d2beb17f138be577a
unknown <vladmain29@gmail.com>
2023-04-15 16:47:05 +0300

    I am tired from this

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,21 +1,33 @@
 package com.epam.mjc;
 
-
+class NotStudentException extends IllegalArgumentException{
+    public NotStudentException(String message, Throwable cause) {
+      super(message, cause);
+    }
+}
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+    try {
+      if (Student.getValueOf(studentID) != null) {
+        return Student.getValueOf(studentID);
+      } else {
+        throw new IllegalArgumentException();
+      }
+    } catch (IllegalArgumentException e) {
+      throw new NotStudentException("Could not find student with ID " + studentID, e);
+    }
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
 
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
-    }
-
+//    for (int i = 0; i < IDs.length; i++) {
+//      Student student = manager.find(IDs[i]);
+//      System.out.println("Student name " + student.getName());
+//    }
+      manager.find(1550);
   }
 }
